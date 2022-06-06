d5f78016d963406f3e3d0e6cdd4181efa564c0d2
Grace <gracenyangayi@gmail.com>
2022-06-06 09:53:22 +0300

    modifie index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+function moreInfo(){
+    document.getElementById("more").innerHTML = "I have also worked as a IT support staff in government office and a data collection cleark at the ministry of agriculture.";
+}
