190efc635e1c868f82ff1335d69caf70a21ae06f
Grace <gracenyangayi@gmail.com>
2022-06-08 11:33:57 +0300

    modify index.html and index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function moreInfo(id){
- document.getElementById("more").innerHTML = "I have also worked as a IT support staff in government office and a data collection cleark at the ministry of agriculture.";
+ document.getElementById("more").innerHTML = "I have also worked as a IT support staff(attachement) in Kenya Forest Service.";
 }
