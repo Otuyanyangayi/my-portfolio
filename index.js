c2215a7b2fe5ba4ec84dc15fffc8856fab9306ad
Grace <gracenyangayi@gmail.com>
2022-06-24 00:01:52 +0300

    update all files

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function moreInfo(id){
  document.getElementById("more").innerHTML = "I have also worked as a IT support staff(attachement) in Kenya Forest Service.";
 }
+
+document.getElementById("submit").addEventListener("click", function(event){
+    event.preventDefault();
+    const comment = document.querySelector('input#comment-input').value
+    const commentsList = document.querySelector('.comments')
+    const p = document.createElement("p");
+    const node = document.createTextNode(comment)
+    p.appendChild(node);
+    commentsList.appendChild(p);
+    document.querySelector('input#comment-input').value = ''
+  });
+
+ 
+  
+  
