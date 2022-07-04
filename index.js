8e21262c6079cce94c9f0461a794ec002f0f7353
Grace <gracenyangayi@gmail.com>
2022-07-04 14:14:39 +0300

    updates

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-function moreInfo(id){
- document.getElementById("more").innerHTML = "I have also worked as a IT support staff(attachement) in Kenya Forest Service.";
-}
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if (this.scrollY > 20) {
+          $('.navbar').addClass("sticky");
+      } else {
+          $('.navbar').removeClass("sticky");
+      }
 
-document.getElementById("submit").addEventListener("click", function(event){
-    event.preventDefault();
-    const comment = document.querySelector('input#comment-input').value
-    const commentsList = document.querySelector('.comments')
-    const p = document.createElement("p");
-    const node = document.createTextNode(comment)
-    p.appendChild(node);
-    commentsList.appendChild(p);
-    document.querySelector('input#comment-input').value = ''
+      if (this.scrollY > 500) {
+          $('.scroll-up-btn').addClass("show");
+      } else {
+          $('.scroll-up-btn').removeClass("show");
+      }
   });
 
- 
-  
-  
+  // slide up script 
+  $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop: 0});
+  });
+
+  // navbar toggle 
+  $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  });
+
+  // typing animation script 
+  var typed = new Typed(".typing", {
+      strings: [  "Front-End developer", " Web Designer", "Network Designer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+      strings: [ "Front-End developer", "Web Designer", "Network Designer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true,
+  })
+
+  // owl carousel script 
+  $('.carousel').owlCarousel({
+      margin: 20,
+      loop: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+          0:{
+              items: 1,
+              nav: false,
+          },
+         /* 600:{
+              items: 2,
+              nav: false,
+          },
+          1000:{
+              items: 3,
+              nav: false,
+          } */
+      }
+  })
+})
